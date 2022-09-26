b469a9260279527a42a72da3d859e4e204ab744a
academy3746 <academy3746@gmail.com>
2022-09-26 11:33:24 +0900

    PHP 기본문법을 공부해보자!

--- ch04/object.php
+++ ch04/object.php
@@ -7,6 +7,8 @@ class MyClass
 
 // #02
 $my_class = new MyClass();
+# -> $instance_name = new ClassName
 
 // #Debugging
-var_dump($my_class); // -> Variable for debugging
+var_dump($my_class);
+# -> Variable for debugging
